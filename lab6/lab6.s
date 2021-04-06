f399c3a81af72b74e6c77bcd2839f8df66ca1621
KWONILCHEOL <kwon6460@gmail.com>
2021-04-06 17:34:26 +0900

    asm

---
+++ lab6/lab6.s
@@ -0,0 +1,110 @@
+st:     .asciz "start\n"
+fmt1:   .asciz "input = "
+fmt11:  .asciz "input(1,2) = "
+fmt2:   .asciz "%d"
+fmt3:   .asciz "output = %d\n"
+        .align 4
+        .global main,printf,scanf
+
+sub1:   save %sp,-96,%sp
+        clr %i2
+		
+sub1lo: mov %i0,%o0
+        call .mul
+        mov %i0,%o1
+
+        add %i2,%o0,%i2
+
+        cmp %i0,%i1
+        bne,a sub1lo
+        inc %i0
+
+        mov %i2,%i1		!i1로 넘김
+
+        ret
+        restore
+
+sub2:   clr %o2
+sub2lo: add %o0,%o2,%o2
+        cmp %o0,%o1
+        bne,a sub2lo
+        inc %o0
+
+        retl
+        mov %o2,%o1		!o1로 전달
+
+main:   save %sp,-96,%sp
+
+input:  set st,%o0
+        call printf
+        nop
+
+        set fmt1,%o0
+        call printf
+        nop
+
+        set fmt2,%o0
+        sub %fp,4,%o1
+        call scanf		!첫 번째
+        nop
+
+        ld [%fp-4],%l0
+
+        cmp %l0,%g0
+        bl exit
+        nop
+
+        set fmt1,%o0
+        call printf
+        nop
+
+        set fmt2,%o0
+        sub %fp,4,%o1
+        call scanf		!두 번째
+        nop
+
+        ld [%fp-4],%l1
+
+third:  set fmt11,%o0
+        call printf
+        nop
+
+        set fmt2,%o0
+        sub %fp,4,%o1
+        call scanf		!세 번째
+        nop
+
+        ld [%fp-4],%l2
+
+        mov %l0,%o0
+        mov %l1,%o1
+
+        cmp %l2,1	!임시 서브루틴1 이동
+        be s1
+        nop
+
+        cmp %l2,2	!임시 서브루틴2 이동
+        be s2
+        nop
+
+        ba third
+        nop
+
+s1:     call sub1	!서브루틴1 이동
+        nop
+        ba print
+        nop
+
+s2:     call sub2	!서브루틴2 이동
+        nop
+
+print:  set fmt3,%o0
+        call printf
+        nop
+
+        mov %o0,%o1
+        ba input
+        nop
+
+exit:   ret
+        restore
---
+++ lab7/lab7.s
@@ -0,0 +1,61 @@
+        .section ".data"
+a:      .single 0r999.0
+b:      .single 0r1.0
+zero:   .double 0r0.0
+fmt0:   .asciz "input = "
+fmt1:   .asciz "%g"
+fmt2:   .asciz "output = %g\n"
+        .align 4
+        .global main,scanf,printf
+
+main:   save %sp,-160,%sp
+
+        set zero,%l0
+        ldd [%l0],%f6   ! f6 = 0.0, f7 = 0.0
+        std %f6,[%fp-16]
+
+input:  set fmt0,%o0
+        call printf
+        nop
+
+        set fmt1,%o0
+        add %fp,-4,%o1
+        call scanf
+        nop
+
+        ld [%fp-4],%f2  ! f2 = input value
+
+        set a,%l0
+        ld [%l0],%f0    ! f0 = 999.0
+        set b,%l0
+        ld [%l0],%f1    ! f1 = 1.0
+
+        fcmps %f2,%f0   ! (input==999) -> output
+        nop
+        fbe output
+        nop
+
+        fcmps %f2,%f1   ! (input<1) -> input
+        nop
+        fbl input
+        nop
+
+        fstod %f2,%f4
+
+        ldd [%fp-16],%f6
+        faddd %f4,%f6,%f6
+        std %f6,[%fp-16]
+
+        ba input
+        nop
+
+output: ldd [%fp-16],%o2
+        mov %o2,%o1
+        mov %o3,%o2
+
+        set fmt2,%o0
+        call printf
+        nop
+
+exit:   ret
+        restore
